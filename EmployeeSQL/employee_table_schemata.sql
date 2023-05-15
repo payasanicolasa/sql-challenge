6dbee50e381bc6d8ca812db828421b6fec1d05da
payasanicolasa <campos.m.nicole@gmail.com>
2023-05-15 16:51:28 -0700

    Updating keys according to feedback

--- EmployeeSQL/employee_table_schemata.sql
+++ EmployeeSQL/employee_table_schemata.sql
@@ -34,12 +34,14 @@ CREATE TABLE dept_emp (
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
-	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY(emp_no)
 );
 
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(5) NOT NULL,
 	emp_no INT NOT NULL,
 	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY(dept_no, emp_no)
 );
